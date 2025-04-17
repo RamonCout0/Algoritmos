db53ab5de46a3f576b833080f57efab3129fda48
Ramon Couto <ramon.couto08.s@gmail.com>
2025-04-17 19:39:56 +0000

    atualizando

--- Aula01/atv2.cpp
+++ Aula01/atv2.cpp
@@ -3,10 +3,7 @@
 int main(){
 int l1, l2;
 
-std::cout << "Digite o valor de l1: ";
 std::cin >> l1;
-
-std::cout << "Digite o valor de l2:";
 std::cin >>l2;
 
 if(l1 >=1 && l1 <= 100000000 && l2 >=1 && l2 <=100000000){
